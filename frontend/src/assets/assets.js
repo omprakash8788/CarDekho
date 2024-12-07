f3570f982d371e134bb9862bfd52747e5fe535dd
omprakash8788 <omprakashkumar552@gmail.com>
2024-12-07 20:02:44 +0530

    Project setup

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,482 @@
+import p_img1 from './car1.webp'
+import p_img2_1 from './car1a.webp'
+import p_img2_2 from './car2.webp'
+import p_img2_3 from './car2a.webp'
+import p_img2_4 from './car3.webp'
+import p_img3 from './car3a.webp'
+import p_img4 from './car3b.webp'
+import p_img5 from './car3bb.webp'
+import p_img6 from './car3bb.webp'
+import p_img7 from './car4.webp'
+import p_img8 from './car4a.webp'
+import p_img9 from './car4aa.webp'
+import p_img10 from './car4bb.webp'
+import p_img11 from './car4bc.webp'
+import p_img12 from './car4cc.webp'
+import p_img13 from './car5.webp'
+import p_img14 from './car5a.webp'
+import p_img15 from './car5b.webp'
+import p_img16 from './car5c.webp'
+import p_img17 from './car5d.webp'
+import p_img18 from './car6a.webp'
+import p_img19 from './car6bb.webp'
+import p_img20 from './car7a.webp'
+import p_img21 from './car7b.webp'
+import p_img22 from './car8a.webp'
+import p_img23 from './car9a.webp'
+import p_img24 from './car9b.webp'
+
+
+// import p_img25 from './p_img25.png'
+// import p_img26 from './p_img26.png'
+// import p_img27 from './p_img27.png'
+// import p_img28 from './p_img28.png'
+// import p_img29 from './p_img29.png'
+// import p_img30 from './p_img30.png'
+// import p_img31 from './p_img31.png'
+// import p_img32 from './p_img32.png'
+// import p_img33 from './p_img33.png'
+// import p_img34 from './p_img34.png'
+// import p_img35 from './p_img35.png'
+// import p_img36 from './p_img36.png'
+// import p_img37 from './p_img37.png'
+// import p_img38 from './p_img38.png'
+// import p_img39 from './p_img39.png'
+// import p_img40 from './p_img40.png'
+// import p_img41 from './p_img41.png'
+// import p_img42 from './p_img42.png'
+// import p_img43 from './p_img43.png'
+// import p_img44 from './p_img44.png'
+// import p_img45 from './p_img45.png'
+// import p_img46 from './p_img46.png'
+// import p_img47 from './p_img47.png'
+// import p_img48 from './p_img48.png'
+// import p_img49 from './p_img49.png'
+// import p_img50 from './p_img50.png'
+// import p_img51 from './p_img51.png'
+// import p_img52 from './p_img52.png'
+
+
+import logo from './logo.png'
+import hero_img from './hero_img.png'
+import cart_icon from './cart_icon.png'
+import bin_icon from './bin_icon.png'
+import dropdown_icon from './dropdown_icon.png'
+import exchange_icon from './exchange_icon.png'
+import profile_icon from './profile_icon.png'
+import quality_icon from './quality_icon.png'
+import search_icon from './search_icon.png'
+import star_dull_icon from './star_dull_icon.png'
+import star_icon from './star_icon.png'
+import support_img from './support_img.png'
+import menu_icon from './menu_icon.png'
+import about_img from './about_img.png'
+import contact_img from './contact_img.png'
+import razorpay_logo from './razorpay_logo.png'
+import stripe_logo from './stripe_logo.png'
+import cross_icon from './cross_icon.png'
+
+export const assets = {
+    logo,
+    hero_img,
+    cart_icon,
+    dropdown_icon,
+    exchange_icon,
+    profile_icon,
+    quality_icon,
+    search_icon,
+    star_dull_icon,
+    star_icon,
+    bin_icon,
+    support_img,
+    menu_icon,
+    about_img,
+    contact_img,
+    razorpay_logo,
+    stripe_logo,
+    cross_icon
+}
+
+// export const products = [
+//     {
+//         _id: "aaaaa",
+//         name: "Women Round Neck Cotton Top",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 100,
+//         image: [p_img1],
+//         category: "Women",
+//         subCategory: "Topwear",
+//         sizes: ["S", "M", "L"],
+//         date: 1716634345448,
+//         bestseller: true
+//     },
+//     {
+//         _id: "aaaab",
+//         name: "Men Round Neck Pure Cotton T-shirt",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 200,
+//         image: [p_img2_1,p_img2_2,p_img2_3,p_img2_4],
+//         category: "Men",
+//         subCategory: "Topwear",
+//         sizes: ["M", "L", "XL"],
+//         date: 1716621345448,
+//         bestseller: true
+//     },
+//     {
+//         _id: "aaaac",
+//         name: "Girls Round Neck Cotton Top",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 220,
+//         image: [p_img3],
+//         category: "Kids",
+//         subCategory: "Topwear",
+//         sizes: ["S", "L", "XL"],
+//         date: 1716234545448,
+//         bestseller: true
+//     },
+//     {
+//         _id: "aaaad",
+//         name: "Men Round Neck Pure Cotton T-shirt",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 110,
+//         image: [p_img4],
+//         category: "Men",
+//         subCategory: "Topwear",
+//         sizes: ["S", "M", "XXL"],
+//         date: 1716621345448,
+//         bestseller: true
+//     },
+//     {
+//         _id: "aaaae",
+//         name: "Women Round Neck Cotton Top",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 130,
+//         image: [p_img5],
+//         category: "Women",
+//         subCategory: "Topwear",
+//         sizes: ["M", "L", "XL"],
+//         date: 1716622345448,
+//         bestseller: true
+//     },
+//     {
+//         _id: "aaaaf",
+//         name: "Girls Round Neck Cotton Top",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 140,
+//         image: [p_img6],
+//         category: "Kids",
+//         subCategory: "Topwear",
+//         sizes: ["S", "L", "XL"],
+//         date: 1716623423448,
+//         bestseller: true
+//     },
+//     {
+//         _id: "aaaag",
+//         name: "Men Tapered Fit Flat-Front Trousers",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 190,
+//         image: [p_img7],
+//         category: "Men",
+//         subCategory: "Bottomwear",
+//         sizes: ["S", "L", "XL"],
+//         date: 1716621542448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaah",
+//         name: "Men Round Neck Pure Cotton T-shirt",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 140,
+//         image: [p_img8],
+//         category: "Men",
+//         subCategory: "Topwear",
+//         sizes: ["S", "M", "L", "XL"],
+//         date: 1716622345448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaai",
+//         name: "Girls Round Neck Cotton Top",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 100,
+//         image: [p_img9],
+//         category: "Kids",
+//         subCategory: "Topwear",
+//         sizes: ["M", "L", "XL"],
+//         date: 1716621235448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaaj",
+//         name: "Men Tapered Fit Flat-Front Trousers",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 110,
+//         image: [p_img10],
+//         category: "Men",
+//         subCategory: "Bottomwear",
+//         sizes: ["S", "L", "XL"],
+//         date: 1716622235448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaak",
+//         name: "Men Round Neck Pure Cotton T-shirt",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 120,
+//         image: [p_img11],
+//         category: "Men",
+//         subCategory: "Topwear",
+//         sizes: ["S", "M", "L"],
+//         date: 1716623345448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaal",
+//         name: "Men Round Neck Pure Cotton T-shirt",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 150,
+//         image: [p_img12],
+//         category: "Men",
+//         subCategory: "Topwear",
+//         sizes: ["S", "M", "L", "XL"],
+//         date: 1716624445448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaam",
+//         name: "Women Round Neck Cotton Top",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 130,
+//         image: [p_img13],
+//         category: "Women",
+//         subCategory: "Topwear",
+//         sizes: ["S", "M", "L", "XL"],
+//         date: 1716625545448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaan",
+//         name: "Boy Round Neck Pure Cotton T-shirt",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 160,
+//         image: [p_img14],
+//         category: "Kids",
+//         subCategory: "Topwear",
+//         sizes: ["S", "M", "L", "XL"],
+//         date: 1716626645448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaao",
+//         name: "Men Tapered Fit Flat-Front Trousers",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 140,
+//         image: [p_img15],
+//         category: "Men",
+//         subCategory: "Bottomwear",
+//         sizes: ["S", "M", "L", "XL"],
+//         date: 1716627745448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaap",
+//         name: "Girls Round Neck Cotton Top",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 170,
+//         image: [p_img16],
+//         category: "Kids",
+//         subCategory: "Topwear",
+//         sizes: ["S", "M", "L", "XL"],
+//         date: 1716628845448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaaq",
+//         name: "Men Tapered Fit Flat-Front Trousers",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 150,
+//         image: [p_img17],
+//         category: "Men",
+//         subCategory: "Bottomwear",
+//         sizes: ["S", "M", "L", "XL"],
+//         date: 1716629945448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaar",
+//         name: "Boy Round Neck Pure Cotton T-shirt",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 180,
+//         image: [p_img18],
+//         category: "Kids",
+//         subCategory: "Topwear",
+//         sizes: ["S", "M", "L", "XL"],
+//         date: 1716631045448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaas",
+//         name: "Boy Round Neck Pure Cotton T-shirt",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 160,
+//         image: [p_img19],
+//         category: "Kids",
+//         subCategory: "Topwear",
+//         sizes: ["S", "M", "L", "XL"],
+//         date: 1716632145448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaat",
+//         name: "Women Palazzo Pants with Waist Belt",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 190,
+//         image: [p_img20],
+//         category: "Women",
+//         subCategory: "Bottomwear",
+//         sizes: ["S", "M", "L", "XL"],
+//         date: 1716633245448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaau",
+//         name: "Women Zip-Front Relaxed Fit Jacket",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 170,
+//         image: [p_img21],
+//         category: "Women",
+//         subCategory: "Winterwear",
+//         sizes: ["S", "M", "L", "XL"],
+//         date: 1716634345448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaav",
+//         name: "Women Palazzo Pants with Waist Belt",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 200,
+//         image: [p_img22],
+//         category: "Women",
+//         subCategory: "Bottomwear",
+//         sizes: ["S", "M", "L", "XL"],
+//         date: 1716635445448,
+//         bestseller: false
+//     },
+//     {
+//         _id: "aaaaw",
+//         name: "Boy Round Neck Pure Cotton T-shirt",
+//         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+//         price: 180,
+//         image: [p_img23],
+//         category: "Kids",
+//         subCategory: "Topwear",
+//         sizes: ["S", "M", "L", "XL"],
+//         date: 1716636545448,
+//         bestseller: false
+//     },
+    
+
+// ]
+
+export const products = [
+    {
+        _id: "car001",
+        model: "Sedan A1",
+        description: "A sleek, fuel-efficient sedan with advanced safety features and a comfortable interior.",
+        price: 20000,
+        image: [p_img1],
+        category: "Sedan",
+        releaseDate: 1716634345448,
+        featured: true
+    },
+    {
+        _id: "car002",
+        model: "SUV X3",
+        description: "A spacious and powerful SUV designed for both urban and off-road adventures.",
+        price: 35000,
+        image: [p_img2_1, p_img2_2, p_img2_3, p_img2_4],
+        category: "SUV",
+        releaseDate: 1716621345448,
+        featured: true
+    },
+    {
+        _id: "car003",
+        model: "Hatchback Z5",
+        description: "A compact car offering excellent mileage and modern features for city driving.",
+        price: 15000,
+        image: [p_img3],
+        category: "Hatchback",
+        releaseDate: 1716234545448,
+        featured: true
+    },
+    {
+        _id: "car004",
+        model: "Convertible C2",
+        description: "A stylish convertible with a powerful engine and an open-top driving experience.",
+        price: 45000,
+        image: [p_img4],
+        category: "Convertible",
+        releaseDate: 1716621345448,
+        featured: true
+    },
+    {
+        _id: "car005",
+        model: "Luxury L1",
+        description: "A premium luxury car offering unparalleled comfort and cutting-edge technology.",
+        price: 60000,
+        image: [p_img5],
+        category: "Luxury",
+        releaseDate: 1716622345448,
+        featured: true
+    },
+    {
+        _id: "car006",
+        model: "Electric E3",
+        description: "An eco-friendly electric vehicle with impressive range and fast charging capabilities.",
+        price: 30000,
+        image: [p_img6],
+        category: "Electric",
+        releaseDate: 1716623423448,
+        featured: true
+    },
+    {
+        _id: "car007",
+        model: "Pickup P7",
+        description: "A rugged pickup truck with high towing capacity and off-road performance.",
+        price: 40000,
+        image: [p_img7],
+        category: "Truck",
+        releaseDate: 1716621542448,
+        featured: false
+    },
+    {
+        _id: "car008",
+        model: "Sports S9",
+        description: "A high-performance sports car with sleek design and exceptional speed.",
+        price: 70000,
+        image: [p_img8],
+        category: "Sports",
+        releaseDate: 1716622345448,
+        featured: false
+    },
+    {
+        _id: "car009",
+        model: "Van V2",
+        description: "A reliable van with ample cargo space and versatile configurations.",
+        price: 25000,
+        image: [p_img9],
+        category: "Van",
+        releaseDate: 1716621235448,
+        featured: false
+    },
+    {
+        _id: "car010",
+        model: "Hybrid H5",
+        description: "A hybrid vehicle combining fuel efficiency with eco-friendly technology.",
+        price: 32000,
+        image: [p_img10],
+        category: "Hybrid",
+        releaseDate: 1716622235448,
+        featured: false
+    }
+];
