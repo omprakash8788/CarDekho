6b7697b05c5bd33d2f12b4a8d14e9a12d6276623
omprakash8788 <omprakashkumar552@gmail.com>
2024-12-29 12:56:12 +0530

    best seller section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import CarRotate from '../components/CarRotate'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <CarRotate/>
       <Hero/>
       <LatestCollection/>
+      <BestSeller/>
     </div>
   )
 }
